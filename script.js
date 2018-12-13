3838047a4103c6e627a34dee341a37a90462479c
KakyoinTheDonut <32132288+KakyoinTheDonut@users.noreply.github.com>
2018-12-13 09:34:16 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+$(".slideOne").click(function() {
+    $(".pic").attr("src", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/Global_warming.jpeg?jQZ6.6nX5EPxX7mIQeIUJjv1nIsm9xHy");
+    $(".caption").show();
+    $(".captionTwo").hide();
+    $(".captionThree").hide();
+     $(".captionFour").hide();
+    
+    });
+    
+$(".slideTwo").click(function() {
+    $(".pic").attr("src", "https://res.cloudinary.com/jerrick/image/upload/f_auto,fl_progressive,q_auto,c_fit,w_1078/iufq6qah7b1rsmhdwjwx");
+    $(".caption").hide();
+    $(".captionTwo").show();
+    $(".captionThree").hide();
+    $(".captionFour").hide();
+    });
+    
+$(".slideThree").click(function() {
+    $(".pic").attr("src", "https://cdn.fleetnews.co.uk/web/1/environment/20181_w268.jpg");
+    $(".caption").hide();
+    $(".captionTwo").hide();
+    $(".captionThree").show();
+    $(".captionFour").hide();
+    
+    });
+    
+$(".slideFour").click(function() {
+    $(".pic").attr("src", "http://public-affairs-trend-lab.s3.amazonaws.com/media/Show%20N%20Tell/follow_the_frog_3.png");
+    $(".caption").hide();
+    $(".captionTwo").hide();
+    $(".captionThree").hide();
+    $(".captionFour").show();
+    
+    });
+    
